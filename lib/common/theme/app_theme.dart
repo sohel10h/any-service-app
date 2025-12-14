bf79861a26d7c10ff4642560462966d2353a78ac
Nabab Khan <programmerjkd@gmail.com>
2025-12-14 11:57:07 +0600

    Update :zap: app text theme from inter to ubuntu

--- lib/common/theme/app_theme.dart
+++ lib/common/theme/app_theme.dart
@@ -10,7 +10,7 @@ class AppTheme {
       colorSchemeSeed: AppColors.primary,
       scaffoldBackgroundColor: AppColors.scaffoldBackground,
       brightness: Brightness.light,
-      textTheme: GoogleFonts.interTextTheme(),
+      textTheme: GoogleFonts.ubuntuTextTheme(),
       appBarTheme: AppBarTheme(
         foregroundColor: AppColors.black,
         backgroundColor: AppColors.scaffoldBackground,
